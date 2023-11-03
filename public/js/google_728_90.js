7325865a1b4efb615102c897fb9254038dd22dce
jekky25 <jekky25@list.ru>
2023-11-03 23:53:05 +0300

    adding javascripts

---
+++ public/js/google_728_90.js
@@ -0,0 +1,9 @@
+google_ad_client = "ca-pub-6379140164632940";
+/* 728x90 шапка (В) */
+google_ad_slot = "8276880722";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+
+$.getScript('http://pagead2.googlesyndication.com/pagead/show_ads.js', function(){
+});
---
+++ public/js/metrika.js
@@ -0,0 +1,8 @@
+<!-- Yandex.Metrika counter -->
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
+<div style="display:none;"><script type="text/javascript">
+try { var yaCounter161110 = new Ya.Metrika({id:161110, enableAll: true, webvisor:true});}
+catch(e) { }
+</script></div>
+<noscript><div><img src="//mc.yandex.ru/watch/161110" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
