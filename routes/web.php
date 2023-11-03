aba1132ed9803a301af688dff3f884322753716b
jekky25 <jekky25@list.ru>
2023-11-03 22:06:42 +0300

    add pr function

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+if (!function_exists('pr')) {
+	function pr (...$ar)
+	{
+		foreach ($ar as $_ar)
+		{
+			echo '<pre>'; print_r ($_ar); echo '</pre>';
+		}
+	}
+}
