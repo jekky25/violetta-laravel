d779192f69d19a387222e0d30286aedf270618b8
jekky25 <jekky25@list.ru>
2023-11-23 02:11:29 +0300

    removing a method from the model

--- app/Models/AnketVisit.php
+++ app/Models/AnketVisit.php
@@ -22,28 +22,6 @@ class AnketVisit extends Model
     	return $items;
 	}
 
-
-	public function getPopul($count = 0, $sex)
-	{
-		$items = self::select(['user_id_prosm', 'id', 'ank_user_id', 'ank_time'])
-		->with('user')
-		->whereHas('user', function ($query) use ($sex) {
-			$query->where('user_sex', $sex);
-		})
-		->groupBy(['user_id_prosm','id', 'ank_user_id', 'ank_time'])
-		->orderBy('user_id_prosm', 'desc')
-		->paginate($count);
-		foreach ($items as &$_item)
-		{
-			$_item->user->user_age 			= Helper::age($_item->user->user_birth_date);
-			$_item->user->user_age_type 	= Helper::ageType($_item->user->user_age);
-			if (count ($_item->user->photo) > 0)
-				$_item->user->photo 		= $_item->user->photo[0];
-		}
-
-		return $items;
-	}
-
 	public function user()
 	{
     	return $this->hasOne(User::class, 'user_id', 'user_id_prosm')
