682ac31cd21a28a964e1fd66d3d51bea9b8ebea0
jekky25 <jekky25@list.ru>
2023-11-23 08:39:49 +0300

    adding a CommentScreenModel

---
+++ app/Models/CommentScreen.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class CommentScreen extends Model
+{
+	use HasFactory;
+
+	protected $table = 'comments_screen_servers';
+
+	public function getByScrId($id)
+	{
+		$items = self::select('*')
+		->where('scr_id', $id)
+		->with ('user')
+		->orderBy('time', 'desc')
+		->get();
+		
+		foreach ($items as &$_item)
+		{
+			if (!empty($_item->user) )
+				$_item->name = $_item->user->user_name;
+			$_item->time = date("d-m-Y",$_item->time);
+		}
+
+		return $items;
+	}
+
+	public function user()
+	{
+    	return $this->belongsTo(User::class, 'user_id', 'user_id');
+	}
+}
