f7596d4783602eafabfdfa4a31995b6d461c0a09
jekky25 <jekky25@list.ru>
2023-12-01 10:01:24 +0300

    adding an interests model

---
+++ app/Models/Interest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Interest extends Model
+{
+	use HasFactory;
+
+	protected $table = 'interest';
+	
+	public function getAll()
+	{
+		$items = self::select('*')
+		->orderBy('name', 'asc')
+		->get();
+		return $items;
+	}
+
+}
