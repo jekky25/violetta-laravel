225698d7617652228f2b18e57437b04ddda1334c
jekky25 <jekky25@list.ru>
2023-12-01 05:53:20 +0300

    adding a meet Target model

---
+++ app/Models/MeetTarget.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class MeetTarget extends Model
+{
+	use HasFactory;
+
+	protected $table = 'target_meet';
+	
+	public function getAll()
+	{
+		$items = self::select('*')
+		->orderBy('name', 'asc')
+		->get();
+		return $items;
+	}
+
+}
