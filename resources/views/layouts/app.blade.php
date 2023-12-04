8a87cf827b4ebc62ed339984dddf85419f2a464b
jekky25 <jekky25@list.ru>
2023-12-04 23:48:13 +0300

    changings css style and fixing bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name='yandex-verification' content='4b22b31a32d44adc' />
 <link rel="Stylesheet"  href="{{ asset('css/reset.css?1') }}" type="text/css" />
-<link rel="Stylesheet"  href="{{ asset('css/style.css?14') }}" type="text/css" />
+<link rel="Stylesheet"  href="{{ asset('css/style.css?17') }}" type="text/css" />
 <script type="text/javascript" src="{{ asset('js/jquery-1.9.0.min.js?1') }}"></script>
 <script async type="text/javascript" src="{{ asset('js/frame_script.js?2') }}"></script>
 {!! $pageMeta !!}
