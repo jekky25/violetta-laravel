dd70a1eba471eeec1da72ec4eba87b9171c219e9
jekky25 <jekky25@list.ru>
2023-12-07 10:16:12 +0300

    adding a model

---
+++ app/Models/Vars.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Vars extends Model
+{
+	use HasFactory;
+
+	protected $table = 'vars';
+
+	public function getAll()
+	{
+		$items = self::select('*')
+		->orderBy('name', 'asc')
+		->get();
+		$vars = [];
+		foreach ($items as $item)
+		{
+			$vars [$item->name] = $item->value;
+		}
+		return $vars;
+	}
+}
