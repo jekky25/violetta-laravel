09b6f866e6b8bc3f7c498858ae24e3cdc571a17e
jekky25 <jekky25@list.ru>
2024-01-04 09:49:39 +0300

    adding confirm template

---
+++ resources/views/mess_die/confirm.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('title', $title)
+@section('main_body')
+<!--main_begin-->
+<table width="100%">
+	<tr height="50" valign="middle">
+		<td align="center">
+			<h1 class="main_title">{{ $msgTitle }}</h1>
+		</td>
+	</tr>
+	<tr valign="top">
+		<td align="center" style="padding-left:30px; padding-right:30px;">
+			<table width="100%">
+				<tr height="150" valign="middle">
+					<td align="center" bgcolor="#fcfbd6" style="border: #fe5804 1px solid;">
+						<form name="anket" action="{{ $confirmAction }}" method="POST">
+							{{ csrf_field() }}
+							<span class="name">{!! $msgText !!}
+								<input type="submit" name="confirm" value="Да" style="width:50px">&nbsp;&nbsp;&nbsp;
+								<input type="submit" name="cancel" value="Нет" style="width:50px">
+							</span>
+						</form>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<!--main_end-->
+@overwrite
