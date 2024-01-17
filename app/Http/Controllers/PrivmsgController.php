efbf3ebd2384df54c91ff1909544e332e88b74a4
jekky25 <jekky25@list.ru>
2024-01-17 09:06:38 +0300

    adding PrivmsgController

---
+++ app/Http/Controllers/PrivmsgController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class PrivmsgController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+}
+
