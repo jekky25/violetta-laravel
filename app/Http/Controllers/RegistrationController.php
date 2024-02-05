d2be4570d8dd77a781bd776e8061cf7e7ea68108
jekky25 <jekky25@list.ru>
2024-02-06 02:47:06 +0300

    adding a registrationController

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class RegistrationController extends Controller
+{
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+
+}
