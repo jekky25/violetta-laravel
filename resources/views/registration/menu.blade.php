8ec330454c3954a7a521fea83cfb8a461623d599
jekky25 <jekky25@list.ru>
2024-02-06 03:29:39 +0300

    adding a menu to the template

---
+++ resources/views/registration/menu.blade.php
@@ -0,0 +1,28 @@
+<ul id="menuReg" class="clear">
+	@if (Route:: currentRouteName() == 'registration.edit')
+	<li class="menuRegAct">Основное</li>
+	@else
+	<li><a href="{{route('registration.edit')}}">Основное</a></li>
+	@endif
+	@if (Route:: currentRouteName() == 'registration.edit.second')
+	<li class="menuRegAct">Дополнительное</li>
+	@else
+	<li><a href="{{route('registration.edit.second')}}">Дополнительное</a></li>
+	@endif
+	@if (Route:: currentRouteName() == 'registration.edit.partner')
+	<li class="menuRegAct">О партнере</li>
+	@else
+	<li><a href="{{route('registration.edit.partner')}}">О партнере</a></li>
+	@endif
+	@if (Route:: currentRouteName() == 'registration.edit.photo')
+	<li class="menuRegAct">Фотографии</li>
+	@else
+	<li><a href="{{route('registration.edit.photo')}}">Фотографии</a></li>
+	@endif
+	@if (Route:: currentRouteName() == 'registration.edit.password')
+	<li class="menuRegAct">Смена пароля</li>
+	@else
+	<li><a href="{{route('registration.edit.password')}}">Смена пароля</a></li>
+	@endif
+	<li><a href="javascript://" onclick="openFadeIFrame({url : '{{route('registration.delete')}}', winWidth : '491', winHeight : '201', width : '460', height : '170' }); return false;">Удалить анкету</a></li>
+</ul>
