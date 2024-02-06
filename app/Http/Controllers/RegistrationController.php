b5646b3514107b7df51e1a0e26c1eafe1fc929e5
jekky25 <jekky25@list.ru>
2024-02-06 09:56:18 +0300

    making registration.edit page

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
+use App\Helpers\Helper;
+use App\Models\Country;
+use App\Models\Region;
+use App\Models\City;
+
 class RegistrationController extends Controller
 {
 
@@ -22,5 +27,33 @@ class RegistrationController extends Controller
         // $this->middleware('auth');
     }
 
+	public function edit (Request $request)
+	{
+		$user 		= Auth::user();
+		$days 		= Helper::getDays();
+		$months 	= Helper::getMonths();
+		$years 		= Helper::getYears();
+		$countries	= Country::getAll();
+		$countryId	= (int) old ('country', $user->user_country);
+		$regionId	= (int) old ('region', $user->user_region);
+		$regions 	= $countryId > 0 	? Region::getByCountryId($countryId) 	: [];
+		$cities 	= $regionId	> 0 	? City::getByRegionId($regionId) 		: [];
 
+		return response()->view ('registration.edit',
+		[
+			'userData'		=> $user,
+			'days'			=> $days,
+			'months'		=> $months,
+			'years'			=> $years,
+			'countries'		=> $countries,
+			'regions'		=> $regions,
+			'cities'		=> $cities,
+		]);
+	}
+
+	public function editPost (Request $request)
+	{
+		return redirect()->back()
+						->withInput();
+	}
 }
--- resources/views/registration/edit.blade.php
+++ resources/views/registration/edit.blade.php
@@ -1,5 +1,121 @@
 @extends('layouts.app')
 @section('title', $title)
 @section('main_body')
-
+<h1 class="mTit">Редактирование данных</h1>
+@include('registration.menu')
+<p class="pad1"></p>
+<h4 class="reg_title">Введите пожалуйста свои данные</h4>
+@if(session('success'))
+<h4 class="reg_title2">информация сохранена</h4>
+@endif
+@if (!empty ($errors->comment->all()))
+<h4 class="reg_title2">данные не сохранены, т. к. не все поля правильно заполнены</h4>
+@endif
+{if !empty($error.err_session)}
+<p class="blue2 pad1">{$error.err_session}</p>
+{/if}
+<p class="pad1"></p>					
+<form name="anketa" action="{{route('registration.edit')}}" method="post">
+{{ csrf_field() }}
+	<h4 class="menu_registration"><div>Имя</div></h4>
+		<p class="pad1 pad2">Укажите Ваше <strong>настоящее имя</strong>. Если вы его не помните, то придумайте псевдоним. Именно по нему (а не по Логину) вас будут узнавать на сайте.</p>
+{if !empty($error.err_name)}
+	<p class="blue2">{$error.err_name}</p>
+{/if}
+		<table class="sexRegForm">
+			<tr>
+				<td rowspan="2"><input class="input3" type="text" name="name" value="{{ old('name', $userData->user_name) }}" /></td>
+				<td class="pad11"><input type="radio" name="sex" value="1"@if (old('sex', $userData->user_sex) == 1) checked="checked"@endif /></td>
+				<td><span class="menuMenReg">Я мужчина</span></td>
+				<td><p class="pad12">дата рождения</p></td>
+			</tr>
+			<tr>
+				<td class="pad11"><input type="radio" name="sex" value="2"@if (old('sex', $userData->user_sex) == 2) checked="checked"@endif /></td>
+				<td><span class="menuWomenReg">Я женщина</span></td>
+				<td>
+					<select name="birth_day" style="width:40px;">
+						@foreach ($days as $item)
+						@if ($item == 0)
+							<option value="0"@if (old('birth_day', $userData->birth_day) == 0) selected="selected"@endif>---</option>
+						@else
+							<option value="{{ $item }}"@if (old('birth_day', $userData->birth_day) == $item) selected="selected"@endif>{{ $item }}</option>
+						@endif
+						@endforeach
+					</select>
+					<select name="birth_month" style="width:80px;">
+						@foreach ($months as $id => $item)
+							<option value="{{ $id }}"@if (old('birth_month', $userData->birth_month) == $id) selected="selected"@endif>{{ $item }}</option>
+						@endforeach
+					</select>
+					<select name="birth_year" style="width:50px;">
+					@foreach ($years as $id => $item)
+					@if ($item == 1900)
+						<option value="{{ $item }}"@if (old('birth_year', $userData->birth_year) == 1990) selected="selected"@endif>---</option>
+					@else
+						<option value="{{ $item }}"@if (old('birth_year', $userData->birth_year) == $item) selected="selected"@endif>{{ $item }}</option>
+					@endif
+					@endforeach
+					</select>
+				</td>
+			</tr>
+		</table>
+		<p class="pad2"></p>
+		<h4 class="menu_registration"><div>Место жительства</div></h4>
+		<p class="pad1 pad2">Укажите <strong>город, регион и страну</strong>, в которой вы живете. Это поможет другим пользователям сайта, которые тоже живут рядом с вами, быстрее вас найти.</p>
+		{if !empty($error.err_place)}
+			<p class="blue2">{$error.err_place}</p>
+		{/if}
+		<table class="cityRegForm">
+			<tr>
+				<td width="150">страна</td>
+				<td><select name="country" id="country" onchange="updateSelect('region', this.value, 'reg');">
+					<option value="0" @if (old('country', $userData->user_country) == 0) selected="selected"@endif>выберите&nbsp;</option>
+					<option value="141" @if (old('country', $userData->user_country) == COUNTRY_ID_RUSSIA) selected="selected"@endif>Россия</option>
+					@foreach ($countries as $item)
+					<option value="{{ $item->id }}" @if (old('country', $userData->user_country) == $item->id && old('country', $userData->user_country) != COUNTRY_ID_RUSSIA) selected="selected"@endif>{{ $item->name }}</option>
+					@endforeach
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td width="150">регион</td>
+			<td><select name="region" id="region" onchange="updateSelect('city', this.value, 'cities');">
+					<option value="0">не важно</option>
+					@if (!empty($regions))
+					@foreach ($regions as $item)
+						<option value="{{ $item->id }}"@if (old('region', $userData->user_region) == $item->id) selected="selected"@endif>{{ $item->name }}</option>
+					@endforeach
+					@endif
+				</select></td>
+		</tr>
+		<tr>
+			<td width="150">город</td>
+			<td><select id="city" name="city">
+					<option value="0">не важно</option>
+					@if (!empty($cities))
+					@foreach ($cities as $item)
+						<option value="{{ $item->id }}"@if (old('city', $userData->user_city) == $item->id) selected="selected"@endif>{{ $item->name }}</option>
+					@endforeach
+					@endif
+				</select>
+			</td>
+		</tr>
+	</table>
+	<script type="text/javascript" src="{{ asset('js/functions_search.js') }}"></script>
+	<p class="pad2"></p>
+	<input type="hidden" name="otsil" value="1" />
+	<p class="pad2"></p>
+	<p class="pad3"><input type="submit" name="sent" class="bgBut8" value="" /></p>
+</form>
+<div class="pad5">					
+	<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6379140164632940";
+/* 468x60 редактирование анкеты */
+google_ad_slot = "8757413983";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+	</script>
+	<script type="text/javascript" async src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+</div>
 @overwrite
