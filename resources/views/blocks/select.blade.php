2d2e2f98070b6009738501f0a6d8884086f3219a
jekky25 <jekky25@list.ru>
2024-02-09 10:33:09 +0300

    adding a property to the select.blade template

--- resources/views/blocks/select.blade.php
+++ resources/views/blocks/select.blade.php
@@ -2,9 +2,9 @@
 	<option value="0">-не важно-</option>
 	@foreach ($obj as $item)
 	@if (!empty($type) && $type == 'I')
-		<option value="{{ $item }}" @if ($item == $UserProp) selected="selected"@endif>{{ $item }}</option>
+		<option value="{{ $item }}" @if ($item == $UserProp) selected="selected"@endif>{{ $item }}@if (!empty($measure)) {{ $measure }}@endif</option>
 	@else
-		<option value="{{ $item->id }}"@if (!empty($item->selected)) selected="selected"@endif>{{ $item->name }}</option>
+		<option value="{{ $item->id }}"@if (!empty($item->selected)) selected="selected"@endif>{{ $item->name }}@if (!empty($measure)) {{ $measure }}@endif</option>
 	@endif
 	@endforeach
 </select>
