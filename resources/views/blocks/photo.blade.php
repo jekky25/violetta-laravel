f3dae5a479cbd1cadda55d34103c2ac8ee67a022
jekky25 <jekky25@list.ru>
2024-03-06 07:47:48 +0300

    fixing bugs

--- resources/views/blocks/photo.blade.php
+++ resources/views/blocks/photo.blade.php
@@ -1,5 +1,5 @@
 <div class="picturEditCont">
-	<a class="dnevBodyPic2" href="{{route('ank.id', $photo->user_id)}}"><img src="{{ App\Helpers\Helper::outPicture($photo->photo_id, $user->user_sex) }}" /></a>
+	<a class="dnevBodyPic2" href="{{route('ank.id', $photo->user_id)}}"><img src="{{ App\Helpers\Helper::outPicture($photo->fotos_id, $user->user_sex) }}" /></a>
 	<a class="editBut3" title="редактировать" href="{{route('registration.edit.photo.edit', $photo->fotos_id)}}"></a>
 	<a class="delBut3" title="удалить" href="{{route('registration.edit.photo.delete', $photo->fotos_id)}}"></a>
 </div>
