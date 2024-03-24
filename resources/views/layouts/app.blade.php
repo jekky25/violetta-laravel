3aa5ac8cbb9c6078e34986d1405731ed888f2367
jekky25 <jekky25@list.ru>
2024-03-24 08:56:38 +0300

    move head block to a separate file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-<title>@yield('title')</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<meta name='yandex-verification' content='4b22b31a32d44adc' />
-<link rel="Stylesheet"  href="{{ asset('css/reset.css?1') }}" type="text/css" />
-<link rel="Stylesheet"  href="{{ asset('css/style.css?19') }}" type="text/css" />
-<script type="text/javascript" src="{{ asset('js/jquery-1.9.0.min.js?1') }}"></script>
-<script async type="text/javascript" src="{{ asset('js/frame_script.js?2') }}"></script>
-{!! $pageMeta !!}
-@stack('scripts')
-</head>
+<head>@include('layouts.head')</head>
 <body>
 <div id="ovHtm">
 	<div id="head">
