0683ad217ce88041e86646f2174ecb1d223608ad
jekky25 <jekky25@list.ru>
2024-04-16 09:47:46 +0300

    add SiteController

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\DreamBook;
+
+class SiteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+		$dreamBook = DreamBook::getAll();
+
+		return response()->view ('site_map', 
+		[
+			'dreamBook' => $dreamBook
+		]);
+    }
+}
+
