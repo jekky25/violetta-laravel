b45a73c72b0026206dd482e30b74b7946f797b4a
jekky25 <jekky25@list.ru>
2024-04-23 08:48:22 +0300

    add forum to the routes

--- routes/web.php
+++ routes/web.php
@@ -124,6 +124,7 @@ Route::middleware('slashes')->group(function () {
 Route::get('ajax/geo.php', 'ajaxController@getGeo');
 Route::get('/', 'HomeController@index')																											->name('home');
 
+Route::get('forum/topic_{$forum_id}_{$topic_id}.html', 'forumController@index')																	->name('forum.topic');
 
 
 Route::get('/migr', function () {
