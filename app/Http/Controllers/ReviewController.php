235cea6528835b4fddbc759b597441d54df06e01
jekky25 <jekky25@list.ru>
2024-05-22 22:09:56 +0300

    make ReviewController

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the review page.
+     * @return void
+     */
+    public function index()
+    {
+      abort (404);
+    }
+}
+
