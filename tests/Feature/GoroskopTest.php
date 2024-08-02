b3813feba765d05d0deff69e214d08c5bbbb0613
jekky25 <jekky25@list.ru>
2024-08-02 09:59:59 +0300

    add unit tests

--- tests/Feature/GoroskopTest.php
+++ tests/Feature/GoroskopTest.php
@@ -5,13 +5,41 @@ use Tests\TestCase;
 
 class GoroskopTest extends TestCase
 {
-    /**
-     * Test a goroskop main page
-     */
-    public function test_goroskop_main_page(): void
-    {
-        $_SERVER['REQUEST_URI'] = '/goroskop.html';
-        $response = $this->get($_SERVER['REQUEST_URI']);
-        $response->assertStatus(200);
-    }
+	/**
+	* Test a goroskop main page
+	*/
+	public function test_goroskop_main_page(): void
+	{
+		$_SERVER['REQUEST_URI'] = '/goroskop.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+	}
+
+	/**
+	* Test goroskop id pages
+	*/
+	public function test_goroskop_id_page(): void
+	{
+		$_SERVER['REQUEST_URI'] = '/goroskop/2.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+
+		$_SERVER['REQUEST_URI'] = '/goroskop/3.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+	}
+
+	/**
+	* Test goroskop type pages
+	*/
+	public function test_goroskop_type_page(): void
+	{
+		$_SERVER['REQUEST_URI'] = '/goroskop/op2.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+
+		$_SERVER['REQUEST_URI'] = '/goroskop/op3.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+	}
 }
