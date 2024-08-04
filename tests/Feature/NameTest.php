bcd582a16d4f75d4d937d01b8bd3e3c6bee67a94
jekky25 <jekky25@list.ru>
2024-08-04 09:10:16 +0300

    add tests to the unit test

--- tests/Feature/NameTest.php
+++ tests/Feature/NameTest.php
@@ -14,4 +14,58 @@ class NameTest extends TestCase
 		$response = $this->get($_SERVER['REQUEST_URI']);
 		$response->assertStatus(200);
 	}
+
+	/**
+	* Test a name gender page
+	*/
+	public function test_name_gender_page(): void
+	{
+		$_SERVER['REQUEST_URI'] = '/names/men.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+		
+		$_SERVER['REQUEST_URI'] = '/names/women.html';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+	}
+
+	/**
+	* Test a name gender literal page
+	*/
+	public function test_name_gender_literal_page(): void
+	{
+		$ar = [
+			'/names/men/3.html',
+			'/names/men/4.html',
+			'/names/women/3.html',
+			'/names/women/4.html',
+		];
+
+		foreach ($ar as $item)
+		{
+			$_SERVER['REQUEST_URI'] = $item;
+			$response = $this->get($_SERVER['REQUEST_URI']);
+			$response->assertStatus(200);
+		}
+	}
+
+
+	/**
+	* Test a name id page
+	*/
+	public function test_name_id_page(): void
+	{
+		$ar = [
+			'/names/61.html',
+			'/names/62.html',
+			'/names/63.html',
+		];
+
+		foreach ($ar as $item)
+		{
+			$_SERVER['REQUEST_URI'] = $item;
+			$response = $this->get($_SERVER['REQUEST_URI']);
+			$response->assertStatus(200);
+		}
+	}
 }
