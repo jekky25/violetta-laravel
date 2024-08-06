7de1651d72e4481034e76f0033aa6f2312ddf076
jekky25 <jekky25@list.ru>
2024-08-06 08:46:54 +0300

    fix bugs

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,25 +3,21 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
 use App\Models\Region;
 use App\Models\City;
 use App\Helpers\Helper;
 
 class AjaxController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
+	/**
+	* Create a new controller instance.
+	*
+	* @return void
+	*/
+	public function __construct()
+	{
+	}
 
 	/**
 	* get city or region
@@ -30,9 +26,8 @@ class AjaxController extends Controller
 	*/
 	public function getGeo (Request $request)
 	{
-
-		$req = $request->post();
-		switch ($req['selectid']) 
+		$selectid = $request->get('selectid');
+		switch ($selectid) 
 		{
 			case 'region':
 				AjaxController::getGeoRegion($request);
@@ -51,10 +46,8 @@ class AjaxController extends Controller
 	*/
 	public function getGeoRegion (Request $request)
 	{
-		$req = $request->post();
-		$id = !empty ($req['id']) ? (int)$req['id'] : 0;
+		$id = (int)$request->get('id');
 		if ($id == 0) return false;
-
 		$regions = Region::getByCountryId($id);
 		Helper::outToXml($regions);
 	}
@@ -66,8 +59,7 @@ class AjaxController extends Controller
 	*/
 	public function getGeoCity (Request $request)
 	{
-		$req = $request->post();
-		$id = !empty ($req['id']) ? (int)$req['id'] : 0;
+		$id = (int)$request->get('id');
 		if ($id == 0) return false;
 
 		$cities = City::getByRegionId($id);
