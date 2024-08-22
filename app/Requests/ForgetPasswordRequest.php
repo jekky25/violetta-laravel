b849748d611a4fbeef1acba4d1c9196304ae6396
jekky25 <jekky25@list.ru>
2024-08-22 06:48:33 +0300

    create forgetPasswordRequest

---
+++ app/Requests/ForgetPasswordRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class ForgetPasswordRequest extends FormRequest
+{
+	/**
+	* replace array errors from default to commit
+	* @param  Illuminate\Contracts\Validation\Validator  $validator
+	* @return void
+	*/
+	public function failedValidation(Validator $validator)
+	{
+		$exception = $validator->getException();
+		$this->errorBag = 'comment';
+        throw (new $exception($validator))
+                    ->errorBag($this->errorBag)
+                    ->redirectTo($this->getRedirectUrl());
+	}
+
+	/**
+	* messages for the request
+	* @return string array
+	*/
+	public function messages():array
+	{
+		return	[
+			'mail.required'			=> 'не указан Е-майл',
+			'mail.email'			=> 'указан некорректный Е-майл'
+		];
+	}
+
+	/**
+	* Get the validation rules that apply to the request.
+	*
+	* @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	*/
+	public function rules(): array
+	{
+		return [
+			'mail'		=> ['required', 'email']
+		];
+	}
+}
