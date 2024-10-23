a6b61ef9f26c5f9b10488853de4ec02c81d8de81
jekky25 <jekky25@list.ru>
2024-10-23 03:16:36 +0300

    remove method from Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -30,30 +30,6 @@ class Helper {
     }
 
 	/**
-	 * make months of the year
-	 *
-	 * @return array
-	 */
-	public static function getMonths()
-	{
-		return [
-			0 	=> '-выберите-',
-			1 	=> 'Января',
-			2 	=> 'Февраля',
-			3 	=> 'Марта',
-			4 	=> 'Апреля',
-			5 	=> 'Мая',
-			6 	=> 'Июня',
-			7 	=> 'Июля',
-			8 	=> 'Августа',
-			9 	=> 'Сентября',
-			10	=> 'Октября',
-			11 	=> 'Ноября',
-			12 	=> 'Декабря'
-		];
-	}
-
-	/**
 	 * make a list of years
 	 *
 	 * @return array
