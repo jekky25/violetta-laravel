553a158d76298f3eb1c89adc7d1d57aff009c61a
jekky25 <jekky25@list.ru>
2025-01-23 08:17:36 +0300

    refactoring update sitemap test

--- tests/Feature/SiteMapTest.php
+++ tests/Feature/SiteMapTest.php
@@ -2,9 +2,22 @@
 
 namespace Tests\Feature;
 use Tests\TestCase;
+use Tests\Traits\hasSetupPrepare;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class SiteMapTest extends TestCase
 {
+	use DatabaseMigrations, hasSetupPrepare;
+
+	/**
+	 * Set up variables
+	 */
+	protected function setUp() :void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+	}
+
 	/**
 	* Test a site map page
 	*/
