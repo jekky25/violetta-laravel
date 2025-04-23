77985e05404db6cd9449bea6a3d2e3c7556c70d8
jekky25 <jekky25@list.ru>
2025-04-23 21:37:56 +0300

    fix bugs in the tests

--- tests/Feature/DiaryTest.php
+++ tests/Feature/DiaryTest.php
@@ -1,22 +1,27 @@
 <?php
 
 namespace Tests\Feature;
+
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\Diary;
+use App\Models\User;
 use Tests\Traits\hasSetupPrepare;
+use Illuminate\Support\Facades\Auth;
 
 class DiaryTest extends TestCase
 {
 	use DatabaseMigrations, hasSetupPrepare;
-	
+
 	/**
 	 * Set up variables
 	 */
-	protected function setUp() :void
+	protected function setUp(): void
 	{
 		parent::setUp();
 		self::setUpPrepare();
+		$user = User::select('id')->first();
+		Auth::loginUsingId($user->id);
 		Diary::factory(50)->create();
 	}
 
@@ -28,8 +33,7 @@ class DiaryTest extends TestCase
 			'/ank/diaries.html?page=3',
 		];
 
-		foreach ($ar as $item)
-		{
+		foreach ($ar as $item) {
 			$_SERVER['REQUEST_URI'] = $item;
 			$response = $this->get($_SERVER['REQUEST_URI']);
 			$response->assertStatus(200);
