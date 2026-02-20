61880dfa05dfa81cc3cc236f2a68f9592a936a8d
jekky25 <jekky25@list.ru>
2026-02-20 08:16:19 +0300

    refactoring a search page

--- resources/views/ankets/search.blade.php
+++ resources/views/ankets/search.blade.php
@@ -19,105 +19,87 @@
 @else
 <h1 class="mTit">Поиск</h1>
 <form name="anketa" action="{{route('search')}}" method="get" class="formSearch">
-<input type="hidden" name="send" value="1" />
-<table class="searchTable">
-	<tr>
-		<td class="right1">я</td>
-		<td>
-			<x-select name=sex :obj="$fields['sex']" :userProp="old('sex')" />
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">ищу</td>
-		<td>
-			<x-select name=find_sex :obj="$fields['findSex']" :userProp="old('find_sex')" />
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">в возрасте</td>
-		<td>от&nbsp; 
-			<x-select name=age_min :obj="$fields['age']" :userProp="old('age_min')" />
-			&nbsp;&nbsp;до&nbsp;
-			<x-select name=age_max :obj="$fields['age']" :userProp="old('age_max')" />
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">страна</td>
-		<td>
-			<x-select name=country id="country" :obj="$fields['country']" :userProp="old('country')">
-				<x-slot:firstInList><option value="141">Россия</option></x-slot>
-				<x-slot:addition>onchange="updateSelect('region', this.value, 'reg');"</x-slot:addition>
-			</x-select>
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">регион</td>
-		<td>
-			<x-select name=region id="region" :obj="[]" :userProp="old('region')">
-				<x-slot:addition>onchange="updateSelect('city', this.value, 'cities');"</x-slot:addition>
-			</x-select>
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">город</td>
-		<td>
-			<x-select name=city id="city" :obj="[]" />
-		</td>
-	</tr>
-</table>
-<p class="pad3"></p>
-<table class="searchTable wth3 pad6">
-	<tr><td class="otobr">Отобрать только</td></tr>	
-</table>
-<table class="searchTable">
-	<tr>
-		<td class="right1">рост</td>
-		<td>
-			от&nbsp; 
-			<x-select name=height_min :obj="$fields['height']" :userProp="old('height_min')" measure="см" />
-			&nbsp;&nbsp;до&nbsp;
-			<x-select name=height_max :obj="$fields['height']" :userProp="old('height_max')" measure="см" />
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">вес</td>
-		<td>
-			от&nbsp; 
-			<x-select name=weight_min :obj="$fields['weight']" :userProp="old('weight_min')" measure="кг" />
-			&nbsp;&nbsp;до&nbsp;
-			<x-select name=weight_max :obj="$fields['weight']" :userProp="old('weight_max')" measure="кг" />
-		</td>
-	</tr>
-	<tr>
-		<td class="right1">телосложение</td>
-		<td><x-select name=body :obj="$fields['body']" :userProp="old('body')" /></td>
-	</tr>
-	<tr>
-		<td class="right1">тип волос</td>
-		<td><x-select name=hair_type :obj="$fields['hairType']" :userProp="old('hair_type')"  /></td>
-	</tr>
-	<tr >
-		<td class="right1">глаза</td>
-		<td><x-select name=eyes :obj="$fields['eyes']" :userProp="old('eyes')"  /></td>
-	</tr>
-	<tr>
-		<td class="right1">только с фото</td>
-		<td><input type="checkbox" name="photo" value="1" /></td>
-	</tr>
-	<tr>
-		<td class="right1">на сайте</td>
-		<td><input type="checkbox" name="online" value="1" /></td>
-	</tr>
-	<tr>
-		<td class="right1">анкет на странице</td>
-		<td>
-			<x-select name=per_page :obj="$fields['perPage']" :userProp="old('per_page')"  />
-		</td>
-	</tr>
-	<tr><td align="center" colspan="2">
+	<input type="hidden" name="send" value="1" />
+	<div class="form-row">
+		<label for="sex">я</label>
+		<x-select name=sex :obj="$fields['sex']" :userProp="old('sex')" id="sex" />
+	</div>
+	<div class="form-row">
+		<label for="find_sex">ищу</label>
+		<x-select id="find_sex" name="find_sex" :obj="$fields['findSex']" :userProp="old('find_sex')" />
+	</div>
+	<div class="form-row">
+		<label>в возрасте</label>
+		<div class="range">
+			<label for="age-min">от</label>
+			<x-select id="age-min" name="age_min" :obj="$fields['age']" :userProp="old('age_min')" />
+			<label for="age-max">до</label>
+			<x-select id="age-max" name="age_max" :obj="$fields['age']" :userProp="old('age_max')" />
+		</div>
+	</div>
+	<div class="form-row">
+		<label for="country">страна</label>
+		<x-select name="country" id="country" :obj="$fields['country']" :userProp="old('country')">
+			<x-slot:firstInList><option value="141">Россия</option></x-slot>
+			<x-slot:addition>onchange="updateSelect('region', this.value, 'reg');"</x-slot:addition>
+		</x-select>
+	</div>
+	<div class="form-row">
+		<label for="region">регион</label>
+		<x-select name="region" id="region" :obj="[]" :userProp="old('region')">
+			<x-slot:addition>onchange="updateSelect('city', this.value, 'cities');"</x-slot:addition>
+		</x-select>
+	</div>
+	<div class="form-row">
+		<label for="city">город</label>
+		<x-select name="city" id="city" :obj="[]" />
+	</div>
+	<div class="line-title-pink mb-1">Отобрать только</div>
+	<div class="form-row">
+		<label>рост</label>
+		<div class="range">
+			<label for="height-min">от</label>
+			<x-select id="height-min" name="height_min" :obj="$fields['height']" :userProp="old('height_min')" measure="см" />
+			<label for="height-max">до</label>
+			<x-select id="height-max" name="height_max" :obj="$fields['height']" :userProp="old('height_max')" measure="см" />
+		</div>
+	</div>
+	<div class="form-row">
+		<label>вес</label>
+		<div class="range">
+			<label for="weight_min">от</label>
+			<x-select id="weight_min" name="weight_min" :obj="$fields['weight']" :userProp="old('weight_min')" measure="кг" />
+			<label for="weight-max">до</label>
+			<x-select id="weight_max" name="weight_max" :obj="$fields['weight']" :userProp="old('weight_max')" measure="кг" />
+		</div>
+	</div>
+	<div class="form-row">
+		<label for="body">телосложение</label>
+		<x-select id="body" name="body" :obj="$fields['body']" :userProp="old('body')" />
+	</div>
+	<div class="form-row">
+		<label for="hair_type">тип волос</label>
+		<x-select id="hair_type" name="hair_type" :obj="$fields['hairType']" :userProp="old('hair_type')"  />
+	</div>
+	<div class="form-row">
+		<label for="eyes">глаза</label>
+		<x-select id="eyes" name="eyes" :obj="$fields['eyes']" :userProp="old('eyes')"  />
+	</div>
+	<div class="form-row form-row-checkbox">
+		<label for="photo">только с фото</label>
+		<input type="checkbox" id="photo" name="photo" value="1" />
+	</div>
+	<div class="form-row form-row-checkbox">
+		<label for="online">на сайте</label>
+		<input type="checkbox" id="online" name="online" value="1" />
+	</div>
+	<div class="form-row">
+		<label for="per_page">анкет на странице</label>
+		<x-select id="per_page" name="per_page" :obj="$fields['perPage']" :userProp="old('per_page')"  />
+	</div>
+	<div class="form-row">
 		<x-submit name=sent value="найти" />
-	</td></tr>
-</table>
+	</div>
 </form>
 <script type="text/javascript" src="{{ asset('js/functions_search.js') }}"></script>
 @endif
