1ec70980340b2f452ec8884da3536e8af54fad14
jekky25 <jekky25@list.ru>
2026-03-23 08:28:23 +0300

    move routes into the group

--- routes/web.php
+++ routes/web.php
@@ -12,64 +12,65 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::middleware('slashes')->group(function () {
-	//Route::get('registration/edit/photo/', 'NoContoller1@index')																				->name('registration.edit.photo');
-	//Route::get('registration/edit/diary/{id}.html', 'NoContoller2@index')												->whereNumber('id')		->name('registration.edit.diary.id');
-	//Route::get('registration/delete/diary/{id}.html', 'NoContoller3@index')												->whereNumber('id')		->name('registration.delete.diary.id');
-	Route::get('registration/edit/diary/', 'RegistrationController@diary')																		->name('registration.edit.diary')			->middleWare('auth');
-	Route::get('registration/edit/settings/', 'RegistrationController@settings')																->name('registration.edit.settings')		->middleWare('auth');
-	Route::put('registration/edit/settings/', 'RegistrationController@settingsPost')															->name('registration.edit.settings.post')	->middleWare('auth');
-	Route::get('registration/top100/', 'RegistrationController@top100')																			->name('registration.top100');
-	Route::post('registration/top100/', 'RegistrationController@top100Post')																	->name('registration.top100.post');
-	Route::get('registration/edit/', 'RegistrationController@edit')																				->name('registration.edit')					->middleWare('auth');
-	Route::put('registration/edit/', 'RegistrationController@post')																				->name('registration.edit.post')			->middleWare('auth');
-	Route::get('registration/edit/second/', 'RegistrationController@second')																	->name('registration.edit.second')			->middleWare('auth');
-	Route::put('registration/edit/second/', 'RegistrationController@secondPost')																->name('registration.edit.second.post')		->middleWare('auth');
-	Route::get('registration/edit/partner/', 'RegistrationController@partner')																	->name('registration.edit.partner')			->middleWare('auth');
-	Route::put('registration/edit/partner/', 'RegistrationController@partnerPost')																->name('registration.edit.partner.post')	->middleWare('auth');
-	Route::get('registration/edit/photo/', 'RegistrationController@photo')																		->name('registration.edit.photo')			->middleWare('auth');
-	Route::post('registration/edit/photo/', 'RegistrationController@photoStore')																->name('registration.edit.photo.post')		->middleWare('auth');
-	Route::get('registration/edit/photo/edit/{id}.html', 'RegistrationController@editPhoto')							->whereNumber('id')		->name('registration.edit.photo.edit')		->middleWare('auth');
-	Route::put('registration/edit/photo/edit/{id}.html', 'RegistrationController@editPhotoUpdate')						->whereNumber('id')		->name('registration.edit.photo.edit.post')	->middleWare('auth');
-	Route::get('registration/edit/photo/delete/{id}.html', 'RegistrationController@destroyPhoto')						->whereNumber('id')		->name('registration.edit.photo.delete')	->middleWare('auth');
-	Route::delete('registration/edit/photo/delete/{id}.html', 'RegistrationController@destroyPhotoAction')				->whereNumber('id')		->name('registration.edit.photo.delete.action')->middleWare('auth');
-	Route::get('registration/edit/pass/', 'RegistrationController@pass')																		->name('registration.edit.password')		->middleWare('auth');
-	Route::put('registration/edit/pass/', 'RegistrationController@passPost')																	->name('registration.edit.password.post')	->middleWare('auth');
-	Route::get('registration/delete/', 'RegistrationController@destroy')																		->name('registration.delete');
-	Route::get('registration/delete/confirm/', 'RegistrationController@destroyConfirm')															->name('registration.delete.confirm')		->middleWare('auth');
-	Route::get('registration/views/', 'AnketController@getViews')																				->name('registration.views')				->middleWare('auth');
+	Route::middleware('auth')->group(function () {
+		Route::get('registration/edit/diary/', 'RegistrationController@diary')													->name('registration.edit.diary');
+		Route::get('registration/edit/settings/', 'RegistrationController@settings')											->name('registration.edit.settings');
+		Route::put('registration/edit/settings/', 'RegistrationController@settingsPost')										->name('registration.edit.settings.post');
+		Route::get('registration/edit/', 'RegistrationController@edit')															->name('registration.edit');
+		Route::put('registration/edit/', 'RegistrationController@post')															->name('registration.edit.post');
+		Route::get('registration/edit/second/', 'RegistrationController@second')												->name('registration.edit.second');
+		Route::put('registration/edit/second/', 'RegistrationController@secondPost')											->name('registration.edit.second.post');
+		Route::get('registration/edit/partner/', 'RegistrationController@partner')												->name('registration.edit.partner');
+		Route::put('registration/edit/partner/', 'RegistrationController@partnerPost')											->name('registration.edit.partner.post');
+		Route::get('registration/edit/photo/', 'RegistrationController@photo')													->name('registration.edit.photo');
+		Route::post('registration/edit/photo/', 'RegistrationController@photoStore')											->name('registration.edit.photo.post');
+		Route::get('registration/edit/photo/edit/{id}.html', 'RegistrationController@editPhoto')		->whereNumber('id')		->name('registration.edit.photo.edit');
+		Route::put('registration/edit/photo/edit/{id}.html', 'RegistrationController@editPhotoUpdate')	->whereNumber('id')		->name('registration.edit.photo.edit.post');
+		Route::get('registration/edit/photo/delete/{id}.html', 'RegistrationController@destroyPhoto')	->whereNumber('id')		->name('registration.edit.photo.delete');
+		Route::delete('registration/edit/photo/delete/{id}.html', 'RegistrationController@destroyPhotoAction')->whereNumber('id')->name('registration.edit.photo.delete.action');
+		Route::get('registration/edit/pass/', 'RegistrationController@pass')													->name('registration.edit.password');
+		Route::put('registration/edit/pass/', 'RegistrationController@passPost')												->name('registration.edit.password.post');
+		Route::get('registration/delete/confirm/', 'RegistrationController@destroyConfirm')										->name('registration.delete.confirm');
+		Route::get('registration/views/', 'AnketController@getViews')															->name('registration.views');
+
+		Route::get('privmsg/', 'PrivmsgController@index')																		->name('privmsg');
+		Route::post('privmsg/delete.html', 'PrivmsgController@destroySelected')													->name('privmsg.delete');
+		Route::delete('privmsg/delete.html', 'PrivmsgController@destroySelectedAction')											->name('privmsg.delete.action');
+		Route::get('privmsg/post/{id}.html', 'PrivmsgController@getAnkMess')							->whereNumber('id')		->name('privmsg.post');
+		Route::get('privmsg/post/delete/{id}.html', 'PrivmsgController@destroy')						->whereNumber('id')		->name('privmsg.post.delete');
+		Route::delete('privmsg/post/delete/{id}.html', 'PrivmsgController@destroyAction')				->whereNumber('id')		->name('privmsg.post.delete.action');
+		Route::post('privmsg/post/add/{id}.html', 'PrivmsgController@store')							->whereNumber('id')		->name('privmsg.post.add');
+
+		Route::get('ank/diary/comment/edit/{id}.html', 'DiaryCommentController@edit')					->whereNumber('id')		->name('ank.diary.comment.edit.id');
+		Route::put('ank/diary/comment/edit/{id}.html', 'DiaryCommentController@update')					->whereNumber('id')		->name('ank.diary.comment.update.id');
+		Route::get('ank/diary/comment/delete/photo/{id}.html', 'DiaryCommentController@destroyPhoto')	->whereNumber('id')		->name('ank.diary.comment.delete.photo.id');
+
+		Route::delete('ank/diary/comment/delete/photo/{id}.html', 'DiaryCommentController@destroyPhotoAction')->whereNumber('id')->name('ank.diary.comment.delete.photo.action.id');
+		Route::get('ank/diary/comment/delete/{id}.html', 'DiaryCommentController@destroy')				->whereNumber('id')		->name('ank.diary.comment.delete.id');
+		Route::delete('ank/diary/comment/delete/{id}.html', 'DiaryCommentController@destroyAction')		->whereNumber('id')		->name('ank.diary.comment.delete.action.id');
+		Route::post('ank/diary/comment/{id}/add.html', 'DiaryCommentController@store')					->whereNumber('id')		->name('ank.diary.comment.add');
+		Route::get('ank/diary/edit/{id}.html', 'DiaryController@edit')									->whereNumber('id')		->name('ank.diary.edit.id');
+		Route::put('ank/diary/edit/{id}.html', 'DiaryController@update')								->whereNumber('id')		->name('ank.diary.edit.update.id');
+		Route::get('ank/diary/delete/photo/{id}.html', 'DiaryController@destroyPhoto')					->whereNumber('id')		->name('ank.diary.delete.photo.id');
+		Route::delete('ank/diary/delete/photo/{id}.html', 'DiaryController@destroyPhotoAction')			->whereNumber('id')		->name('ank.diary.delete.photo.action.id');
+		Route::get('ank/diary/delete/{id}.html', 'DiaryController@destroy')								->whereNumber('id')		->name('ank.diary.delete.id');
+		Route::delete('ank/diary/delete/{id}.html', 'DiaryController@destroyAction')					->whereNumber('id')		->name('ank.diary.delete.action.id');
+		Route::post('ank/diary/add.html', 'DiaryController@store')																->name('ank.diary.add');
+		Route::get('ank/photo/{id}.html', 'AnkController@getMainPhoto')									->whereNumber('id')		->name('ank.photo.id');
+		Route::get('ank/f/photo_{id}/', 'AnkController@getPhoto')										->whereNumber('id')		->name('ank.photo.photo_id');
+
+		Route::get('logout/', 'AuthController@logout')													->name('logout');
+	});
+
+	Route::get('registration/top100/', 'RegistrationController@top100')															->name('registration.top100');
+	Route::post('registration/top100/', 'RegistrationController@top100Post')													->name('registration.top100.post');
+	Route::get('registration/delete/', 'RegistrationController@destroy')														->name('registration.delete');
 	Route::get('registration/', 'RegistrationController@registration')																			->name('registration')						->middleWare('guest');
 	Route::post('registration/', 'RegistrationController@registrationStore')																	->name('registration.post');
 	Route::get('registration/confirm/{id}/{code}/', 'RegistrationController@confirm')									->whereNumber('id')		->name('registration.confirm');
 
-	Route::get('privmsg/', 'PrivmsgController@index')																							->name('privmsg')							->middleWare('auth');
-	Route::post('privmsg/delete.html', 'PrivmsgController@destroySelected')																		->name('privmsg.delete')					->middleWare('auth');
-	Route::delete('privmsg/delete.html', 'PrivmsgController@destroySelectedAction')																->name('privmsg.delete.action')				->middleWare('auth');
-	Route::get('privmsg/post/{id}.html', 'PrivmsgController@getAnkMess')												->whereNumber('id')		->name('privmsg.post')						->middleWare('auth');
-	Route::get('privmsg/post/delete/{id}.html', 'PrivmsgController@destroy')											->whereNumber('id')		->name('privmsg.post.delete')				->middleWare('auth');
-	Route::delete('privmsg/post/delete/{id}.html', 'PrivmsgController@destroyAction')									->whereNumber('id')		->name('privmsg.post.delete.action')		->middleWare('auth');
-	Route::post('privmsg/post/add/{id}.html', 'PrivmsgController@store')												->whereNumber('id')		->name('privmsg.post.add')					->middleWare('auth');
-
 	Route::get('ank/diary/comments/{id}.html', 'DiaryCommentController@index')											->whereNumber('id')		->name('ank.diary.comments');
-	Route::get('ank/diary/comment/edit/{id}.html', 'DiaryCommentController@edit')										->whereNumber('id')		->name('ank.diary.comment.edit.id')			->middleWare('auth');
-	Route::put('ank/diary/comment/edit/{id}.html', 'DiaryCommentController@update')										->whereNumber('id')		->name('ank.diary.comment.update.id')		->middleWare('auth');
-	Route::get('ank/diary/comment/delete/photo/{id}.html', 'DiaryCommentController@destroyPhoto')						->whereNumber('id')		->name('ank.diary.comment.delete.photo.id')	->middleWare('auth');
-	Route::delete('ank/diary/comment/delete/photo/{id}.html', 'DiaryCommentController@destroyPhotoAction')				->whereNumber('id')		->name('ank.diary.comment.delete.photo.action.id')
-																																															->middleWare('auth');
-	Route::get('ank/diary/comment/delete/{id}.html', 'DiaryCommentController@destroy')									->whereNumber('id')		->name('ank.diary.comment.delete.id')		->middleWare('auth');
-	Route::delete('ank/diary/comment/delete/{id}.html', 'DiaryCommentController@destroyAction')							->whereNumber('id')		->name('ank.diary.comment.delete.action.id')->middleWare('auth');
-	Route::post('ank/diary/comment/{id}/add.html', 'DiaryCommentController@store')										->whereNumber('id')		->name('ank.diary.comment.add')				->middleWare('auth');
-	Route::get('ank/diary/edit/{id}.html', 'DiaryController@edit')														->whereNumber('id')		->name('ank.diary.edit.id')					->middleWare('auth');
-	Route::put('ank/diary/edit/{id}.html', 'DiaryController@update')													->whereNumber('id')		->name('ank.diary.edit.update.id')			->middleWare('auth');
-	Route::get('ank/diary/delete/photo/{id}.html', 'DiaryController@destroyPhoto')										->whereNumber('id')		->name('ank.diary.delete.photo.id')			->middleWare('auth');
-	Route::delete('ank/diary/delete/photo/{id}.html', 'DiaryController@destroyPhotoAction')								->whereNumber('id')		->name('ank.diary.delete.photo.action.id')	->middleWare('auth');
-	Route::get('ank/diary/delete/{id}.html', 'DiaryController@destroy')													->whereNumber('id')		->name('ank.diary.delete.id')				->middleWare('auth');
-	Route::delete('ank/diary/delete/{id}.html', 'DiaryController@destroyAction')										->whereNumber('id')		->name('ank.diary.delete.action.id')		->middleWare('auth');
 	Route::get('ank/diary/{id}.html', 'DiaryController@show')															->whereNumber('id')		->name('ank.diary.id');
-	Route::post('ank/diary/add.html', 'DiaryController@store')																					->name('ank.diary.add')						->middleWare('auth');
-	//Route::get('ank/photo/f/{id}.html', 'NoContoller12@index')															->whereNumber('id')		->name('ank.photo.f.id');
-	Route::get('ank/photo/{id}.html', 'AnkController@getMainPhoto')														->whereNumber('id')		->name('ank.photo.id')						->middleWare('auth');
-
-	Route::get('ank/f/photo_{id}/', 'AnkController@getPhoto')															->whereNumber('id')		->name('ank.photo.photo_id')				->middleWare('auth');
 	Route::get('ank/f/{id}/', 'AnkController@getAnk')																	->whereNumber('id')		->name('ank.full.id');
 	Route::get('ank/{id}/', 'AnkController@getAnk')																		->whereNumber('id')		->name('ank.id');
 	Route::get('ankets/{sex}/{age}/', 'AnketController@getAnkets')														->where('sex', '(men|women)')
@@ -113,7 +114,6 @@ Route::middleware('slashes')->group(function () {
 
 	Route::get('search/', 'AnketController@getBySearch')																						->name('search');
 	
-	Route::get('logout/', 'AuthController@logout')																								->name('logout')->middleWare('auth');
 	Route::post('login/', 'AuthController@login')																								->name('login');
 	Route::get('login/', function () {
 		return redirect()->route('home');
