162386dcfcedcc171d7762dcf62ea90713ef52ad
jekky25 <jekky25@list.ru>
2026-04-05 08:01:32 +0300

    add a Unit Test

---
+++ tests/Feature/CommentScreenControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Tests\TestCase;
+use App\Services\CommentScreenService;
+use Tests\Traits\hasSetupPrepare;
+use App\Models\User;
+use Mockery;
+
+class CommentScreenControllerTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected function setUp(): void
+	{
+		parent::$migrated = true;
+		parent::setUp();
+		self::setUpPrepare();
+	}
+
+	public function test_store_creates_comment()
+	{
+		$_SERVER['REQUEST_URI'] = route('screensavers.id', 1);
+		$service = Mockery::mock(CommentScreenService::class);
+		$service->shouldReceive('store')->once();
+
+		$this->app->instance(CommentScreenService::class, $service);
+
+		$user = User::factory()->create();
+
+		$response = $this->actingAs($user)->post($_SERVER['REQUEST_URI'], [
+			'description' => 'test'
+		]);
+
+		$response->assertRedirect();
+	}
+}
