a0767053e6e3602af6cac710fa1cc5fbf13e34c2
jekky25 <jekky25@list.ru>
2026-04-09 16:40:59 +0300

    refactoring SettingsRequest

--- app/Requests/SettingRequest.php
+++ app/Requests/SettingRequest.php
@@ -3,40 +3,9 @@
 namespace App\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Contracts\Validation\Validator;
 
 class SettingRequest extends FormRequest
 {
-	/**
-	 * replace array errors from default to commit
-	 * @param  Illuminate\Contracts\Validation\Validator  $validator
-	 * @return void
-	 */
-	public function failedValidation(Validator $validator)
-	{
-		$exception = $validator->getException();
-		$this->errorBag = 'comment';
-		throw (new $exception($validator))
-			->errorBag($this->errorBag)
-			->redirectTo($this->getRedirectUrl());
-	}
-
-	/**
-	 * Prepare params for validation
-	 *
-	 * @return void
-	 */
-	protected function prepareForValidation()
-	{
-		$this->merge([
-			'dont_send_email' 		=> !empty($this->dont_send_email) ? (int) $this->dont_send_email : 0,
-			'refresh_date'			=> date("Y-m-d"),
-			'refresh_date_t'		=> time(),
-			'session_time'			=> time(),
-			'lastvisit'				=> time()
-		]);
-	}
-
 	/**
 	 * Get the validation rules that apply to the request.
 	 *
@@ -45,11 +14,7 @@ class SettingRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			'dont_send_email'			=> ['integer'],
-			'refresh_date'				=> ['string'],
-			'refresh_date_t'			=> ['integer'],
-			'session_time'				=> ['integer'],
-			'lastvisit'					=> ['integer']
+			'dont_send_email'			=> ['integer']
 		];
 	}
 }
