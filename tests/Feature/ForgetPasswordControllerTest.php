3ff6f111a0890bbf295cb05ebf3b12d16791d292
jekky25 <jekky25@list.ru>
2026-04-10 21:24:22 +0300

    add a Unit test

---
+++ tests/Feature/ForgetPasswordControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Tests\TestCase;
+use App\Services\ForgetPasswordService;
+use Tests\Traits\hasSetupPrepare;
+use Mockery;
+
+class ForgetPasswordControllerTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+	}
+
+    public function test_send_password_calls_service_and_redirects(): void
+    {
+        $service = Mockery::mock(ForgetPasswordService::class);
+        $this->app->instance(ForgetPasswordService::class, $service);
+
+        $service->shouldReceive('sendPassword')
+            ->once()
+            ->with(Mockery::on(function ($dto) {
+                return method_exists($dto, 'toArray');
+            }));
+
+
+        $_SERVER['REQUEST_URI'] = route('forget_pass.post', ['email' => 'test@mail.com']);
+        $response = $this->post($_SERVER['REQUEST_URI']);
+
+        $response->assertRedirect();
+        $response->assertSessionHas('success');
+    }
+}
---
+++ tests/Feature/ForgetPasswordServiceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use App\Services\ForgetPasswordService;
+use App\Interfaces\UserInterface;
+use App\DTO\ForgetPasswordDTO;
+use App\Models\User;
+use App\Mail\ForgetPasswordEmail;
+use Illuminate\Support\Facades\Mail;
+use Tests\Traits\hasSetupPrepare;
+use Mockery;
+
+class ForgetPasswordServiceTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+	}
+
+    public function test_send_password_sends_email_if_user_exists(): void
+    {
+        Mail::fake();
+
+        $repository = Mockery::mock(UserInterface::class);
+        $service = new ForgetPasswordService($repository);
+
+        $user = User::factory()->make([
+            'email' => 'test@mail.com'
+        ]);
+
+        $repository->shouldReceive('getByEmail')
+            ->once()
+            ->with('test@mail.com')
+            ->andReturn($user);
+
+		$dto = new ForgetPasswordDTO('test@mail.com');
+        $service->sendPassword($dto);
+
+        Mail::assertSent(ForgetPasswordEmail::class, function ($mail) {
+            return $mail->hasTo('test@mail.com');
+        });
+    }
+
+    public function test_send_password_does_nothing_if_user_not_found(): void
+    {
+        Mail::fake();
+
+        $repository = Mockery::mock(UserInterface::class);
+        $service = new ForgetPasswordService($repository);
+
+        $repository->shouldReceive('getByEmail')
+            ->once()
+            ->andReturn(null);
+
+		$dto = new ForgetPasswordDTO('notfound@mail.com');
+        $service->sendPassword($dto);
+
+        Mail::assertNothingSent();
+    }
+}
