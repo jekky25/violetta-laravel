5e4b6f37e3311013a0808936d318e5d942d0ea36
jekky25 <jekky25@list.ru>
2026-05-20 17:55:20 +0300

    add a Unit test

---
+++ tests/Feature/ApiAuthControllerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Tests\TestCase;
+use Mockery;
+use App\Models\User;
+use App\Services\ApiAuthService;
+use Tests\Traits\hasSetupPrepare;
+use \App\Http\Resources\Profile\AuthProfileResource;
+
+class ApiAuthControllerTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected $service;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+
+		$this->service = Mockery::mock(ApiAuthService::class);
+
+		$this->app->instance(ApiAuthService::class, $this->service);
+	}
+
+	public function test_me_returns_null_if_user_not_authenticated()
+	{
+		$_SERVER['REQUEST_URI'] = route('auth.get');
+		$response = $this->getJson($_SERVER['REQUEST_URI']);
+
+		$response->assertOk();
+		$this->assertEquals('', $response->getContent());
+	}
+
+	public function test_me_returns_authenticated_user()
+	{
+		$user = User::factory()->create();
+
+		$this->actingAs($user);
+
+		$_SERVER['REQUEST_URI'] = route('auth.get');
+		$response = $this->getJson($_SERVER['REQUEST_URI']);
+
+		$response->assertOk();
+
+		$response->assertJsonFragment([
+			'id' => $user->id,
+		]);
+	}
+
+	public function test_login_returns_user_resource()
+	{
+		$user = User::factory()->create();
+
+		$data = [
+			'login' => 'test',
+			'password' => '123456'
+		];
+
+		$resource = new AuthProfileResource($user);
+
+		$this->service->shouldReceive('loginApi')
+			->once()
+			->with($data)
+			->andReturn($resource);
+
+		$_SERVER['REQUEST_URI'] = route('login.api');
+
+		$response = $this->postJson($_SERVER['REQUEST_URI'], $data);
+		$response->assertStatus(201);
+	}
+}
---
+++ tests/Feature/ApiAuthServiceTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use Mockery;
+use App\Models\User;
+use App\Services\ApiAuthService;
+use Illuminate\Support\Facades\Auth;
+use App\Interfaces\UserInterface;
+use App\Http\Resources\Profile\AuthProfileResource;
+use Illuminate\Http\Response;
+use Tests\Traits\hasSetupPrepare;
+
+class ApiAuthServiceTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected $repository;
+	protected ApiAuthService $service;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+
+		$this->repository = Mockery::mock(UserInterface::class);
+
+		$this->service = new ApiAuthService(
+			$this->repository
+		);
+	}
+
+	public function test_login_api_returns_user_resource()
+	{
+		$user = User::factory()->make([
+			'id' => 1
+		]);
+
+		$data = [
+			'login' => 'test',
+			'password' => '123456'
+		];
+
+		$this->repository->shouldReceive('getByLoginAndPass')
+			->once()
+			->with('test', '123456')
+			->andReturn($user);
+
+		Auth::shouldReceive('login')
+			->once()
+			->with($user, true);
+
+		$result = $this->service->loginApi($data);
+
+		$this->assertInstanceOf(
+			AuthProfileResource::class,
+			$result
+		);
+	}
+
+	public function test_login_api_returns_400_if_user_not_found()
+	{
+		$data = [
+			'login' => 'wrong',
+			'password' => 'wrong'
+		];
+
+		$this->repository->shouldReceive('getByLoginAndPass')
+			->once()
+			->with('wrong', 'wrong')
+			->andReturn(null);
+
+		$result = $this->service->loginApi($data);
+
+		$this->assertInstanceOf(Response::class, $result);
+
+		$this->assertEquals(400, $result->status());
+	}
+}
