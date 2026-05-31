950197913a4ada853f24a59f5cc66c68f54a712f
jekky25 <jekky25@list.ru>
2026-05-31 16:40:03 +0300

    add Unit Tests

---
+++ tests/Feature/ForumControllerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Tests\TestCase;
+use Mockery;
+use App\Services\ForumService;
+use Tests\Traits\hasSetupPrepare;
+
+class ForumControllerTest extends TestCase
+{
+	use hasSetupPrepare;
+
+    protected $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        self::setUpPrepare();
+
+        $this->service = Mockery::mock(
+            ForumService::class
+        );
+
+        $this->app->instance(
+            ForumService::class,
+            $this->service
+        );
+    }
+
+    public function test_index_returns_false()
+    {
+        $controller = app(
+            \App\Http\Controllers\ForumController::class
+        );
+
+        $this->assertFalse(
+            $controller->index()
+        );
+    }
+
+    public function test_get_top_returns_resource_collection()
+    {
+        $topics = [
+           (object) [
+                'topic_title' => 'Topic 1',
+                'topic_id' => 1,
+                'forum_id' => 2
+            ],
+           (object) [
+                'topic_title' => 'Topic 2',
+                'topic_id' => 2,
+                'forum_id' => 2
+            ]
+        ];
+
+        $this->service
+            ->shouldReceive('getTop')
+            ->once()
+            ->andReturn($topics);
+
+        $response = $this->getJson(
+            route('forum.get.top')
+        );
+
+        $response->assertOk();
+
+        $response->assertJsonCount(2);
+    }
+}
---
+++ tests/Feature/ForumServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+use Mockery;
+use App\Services\ForumService;
+use App\Interfaces\ForumInterface;
+use Tests\Traits\hasSetupPrepare;
+
+class ForumServiceTest extends TestCase
+{
+	use hasSetupPrepare;
+
+	protected $repository;
+
+	protected ForumService $service;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		self::setUpPrepare();
+
+		$this->repository = Mockery::mock(
+			ForumInterface::class
+		);
+
+		$this->service = new ForumService(
+			$this->repository
+		);
+	}
+
+	public function test_get_top_returns_topics()
+	{
+		$topics = [
+			[
+				'id' => 1,
+				'title' => 'Topic 1'
+			],
+			[
+				'id' => 2,
+				'title' => 'Topic 2'
+			]
+		];
+
+		$this->repository
+			->shouldReceive('getTop')
+			->once()
+			->andReturn($topics);
+
+		$result = $this->service->getTop();
+
+		$this->assertEquals(
+			$topics,
+			$result
+		);
+	}
+}
